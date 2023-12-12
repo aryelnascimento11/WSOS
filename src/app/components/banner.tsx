25680c77b467d3e99df764aeacb3330ea3b04331
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-12-12 16:36:09 -0300

    1commit

---
+++ src/app/components/banner.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Banner(){
+    return(
+        <section className="">
+            <div className="flex gap-4" >
+                <div className="w-96 h-64 border border-black rounded "></div>
+                <div className="w-96 h-64 border border-black rounded"></div>
+            </div>
+            <br />
+            <div className="flex gap-4" >
+                <div className="w-96 h-64 border border-black rounded "></div>
+                <div className="w-96 h-64 border border-black rounded"></div>
+            </div>
+            <br/>
+            <div className="flex gap-4" >
+                <div className="w-96 h-64 border border-black rounded "></div>
+                <div className="w-96 h-64 border border-black rounded"></div>
+            </div>
+            <br/>
+            <div className="flex gap-4" >
+                <div className="w-96 h-64 border border-black rounded "></div>
+                <div className="w-96 h-64 border border-black rounded"></div>
+            </div>
+            
+            
+        </section>    
+    )
+   
+}
